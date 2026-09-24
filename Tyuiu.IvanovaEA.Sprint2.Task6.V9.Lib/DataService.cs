c12053c7d977844c6d8fe62bde7a03e9fe92c602
“Екатерина <kataivanova0380@gmail.com>
2026-09-24 22:44:47 +0500

    add task6

---
+++ Tyuiu.IvanovaEA.Sprint2.Task6.V9.Lib/DataService.cs
@@ -0,0 +1,44 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+//  Условие: Дата некоторого дня характеризуется двумя натуральными числами: m(порядковый номер месяца) и n(число). 
+//    По заданным n и m определить дату следующего дня (принять, что n и m не характеризуют 31 декабря).
+
+namespace Tyuiu.IvanovaEA.Sprint2.Task6.V9.Lib
+{
+    public class DataService : ISprint2Task6V9
+    {
+        public string FindDateOfNextDay(int m, int n)
+        {
+            switch (m)
+            {
+                // Месяцы с 31 днём
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                    return n < 31 ? $"{n + 1:D2}.{m:D2}" : $"01.{m + 1:D2}";
+    
+
+                case 12:
+                    // 31 декабря
+                    return $"{n + 1:D2}.12";
+
+                // Месяцы с 30 днями
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    return n < 30 ? $"{n + 1:D2}.{m:D2}" : $"01.{m + 1:D2}";
+
+                // Февраль
+                case 2:
+                    return n < 28 ? $"{n + 1:D2}.02" : "01.03";
+
+                default:
+                    return "Месяц должен быть от 1 до 12!";
+            }
+        }
+    }
+}
---
+++ Tyuiu.IvanovaEA.Sprint2.Task6.V9.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.IvanovaEA.Sprint2.Task6.V9.Lib;
+
+namespace Tyuiu.IvanovaEA.Sprint2.Task6.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidFindDateOfNextDay()
+        {
+            DataService ds = new DataService();
+
+            // 28 февраля - 1 марта
+            Assert.AreEqual("01.03", ds.FindDateOfNextDay(2, 28));
+
+            // 31 января - 1 февраля
+            Assert.AreEqual("01.02", ds.FindDateOfNextDay(1, 31));
+
+            // 30 апреля - 1 мая
+            Assert.AreEqual("01.05", ds.FindDateOfNextDay(4, 30));
+
+            // 30 декабря - 31 декабря
+            Assert.AreEqual("31.12", ds.FindDateOfNextDay(12, 30));
+        }
+    }
+}
